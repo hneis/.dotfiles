8c564a8b86b7cd2b4b5ccb4ba07993f36b41080d
Ivanyuta Dmitriy <d.ivaniuta@metrika72.ru>
2024-08-29 09:42:13 +0500

    update hypr

--- hypr/initial-boot.sh
+++ hypr/initial-boot.sh
@@ -8,7 +8,7 @@
 # Variables
 scriptsDir=$HOME/.config/hypr/scripts
 wallpaper=$HOME/.config/hypr/wallpaper_effects/.wallpaper_modified
-waybar_style="$HOME/.config/waybar/style/[Wallust] Box type.css"
+waybar_style="$HOME/.config/waybar/style/[Dark] Half-Moon.css"
 kvantum_theme="Catppuccin-Mocha"
 color_scheme="prefer-dark"
 gtk_theme="Andromeda-dark"
@@ -29,11 +29,11 @@ if [ ! -f ~/.config/hypr/.initial_startup_done ]; then
   fi
 
   # initiate GTK dark mode and apply icon and cursor theme
-  # gsettings set org.gnome.desktop.interface color-scheme $color_scheme > /dev/null 2>&1 &
-  # gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme > /dev/null 2>&1 &
-  # gsettings set org.gnome.desktop.interface icon-theme $icon_theme > /dev/null 2>&1 &
-  # gsettings set org.gnome.desktop.interface cursor-theme $cursor_theme > /dev/null 2>&1 &
-  # gsettings set org.gnome.desktop.interface cursor-size 24 > /dev/null 2>&1 &
+  gsettings set org.gnome.desktop.interface color-scheme $color_scheme >/dev/null 2>&1 &
+  gsettings set org.gnome.desktop.interface gtk-theme $gtk_theme >/dev/null 2>&1 &
+  gsettings set org.gnome.desktop.interface icon-theme $icon_theme >/dev/null 2>&1 &
+  gsettings set org.gnome.desktop.interface cursor-theme $cursor_theme >/dev/null 2>&1 &
+  gsettings set org.gnome.desktop.interface cursor-size 24 >/dev/null 2>&1 &
 
   # initiate kvantum theme
   kvantummanager --set "$kvantum_theme" >/dev/null 2>&1 &
